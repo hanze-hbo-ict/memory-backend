e52d588fa08c0c8e9211cfac61bdc1adcf278d44
Bart Barnard <b.barnard@pl.hanze.nl>
2022-06-10 08:14:51 +0200

    Claims aan de payload toegevoegd 🥳

--- src/EventListener/JWTCreatedListener.php
+++ src/EventListener/JWTCreatedListener.php
@@ -19,9 +19,14 @@ class JWTCreatedListener {
     public function onJWTCreated(JWTCreatedEvent $event)
     {
         $payload = $event->getData();
+        $payload['sub'] = $event->getUser()->id;
+        $payload['iss'] = 'memory backend';
 
         $event->setData($payload);
 
+        // @Deprecated
+        // foutje, deze data moet in de payload, niet in de header.
+        // dit zit er nog in voor backward compatibility
         //https://www.rfc-editor.org/rfc/rfc7519#section-4.1
         $header = $event->getHeader();
         $header['sub'] =  $event->getUser()->id;
